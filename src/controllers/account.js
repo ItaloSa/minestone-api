50196baa3b840d03f5de825ba4c8b77a6e25667d
Italo <italusousa@gmail.com>
2020-06-09 19:18:03 -0300

    account verification

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+
 const Rcon = require('../config/rcon');
 const Database = require('../config/database');
+const Email = require('../services/email');
+
 const CustomError = require('../helpers/Errors/CustomError');
+
+const { SERVICE_URL, EMAIL_USER } = process.env;
 
 const register = async (data) => {
   try {
@@ -8,10 +14,10 @@ const register = async (data) => {
     const users = db.get('users');
 
     const existsUser = users
-      .find({ userName: data.userName })
+      .find({ userName: data.userName, verified: true })
       .value();
     const existsEmail = users
-      .find({ email: data.email })
+      .find({ email: data.email, verified: true })
       .value();
 
     if (existsUser) {
@@ -20,26 +26,75 @@ const register = async (data) => {
       throw new CustomError('EMAIL_IN_USE', 200);
     }
 
-    const lastId = users.value().length;
-    const now = new Date().toISOString();
+    const lastId = users
+      .size()
+      .value();
+    const now = new Date();
+
     const newUser = {
       ...data,
       id: lastId + 1,
-      createdAt: now,
-      updatedAt: now,
+      createdAt: now.toISOString(),
+      verified: false
     };
 
-    const rcon = await Rcon.instance();
-    const response = await rcon.send(`whitelist add ${newUser.userName}`);
+    let verifToken = {
+      uid: newUser.id,
+      exp: new Date(now.getTime() + 2 * 3600000).toISOString()
+    };
 
-    if (response.includes('already')) {
-      throw new CustomError('USER_EXITS', 200);
-    } else if (response.includes('Added')) {
-      users.push(newUser).write();
-      return newUser;
-    } else {
+    newUser.token = Buffer.from(JSON.stringify(verifToken)).toString('base64');
+
+    const template = fs.readFileSync(
+      `${__dirname}/../services/email/templates/main.html`, 'utf-8'
+    );
+    await Email.send(template, {
+      from: `"MineStone" <${EMAIL_USER}>`,
+      to: newUser.email,
+      subject: 'Confirm your account'
+    }, {
+      player: newUser.userName,
+      link: `${SERVICE_URL}/account/verify/${newUser.token}`
+    });
+
+    users.push(newUser).write();
+    return newUser;
+
+  } catch (err) {
+    throw err;
+  }
+};
+
+const confirmAccount = async (token) => {
+  try {
+
+    const db = Database.instance();
+    const users = db.get('users');
+
+    const user = users
+      .find({ token: token })
+      .value();
+    console.log(token, user)
+    if (!user) {
+      return;
+    }
+
+    const payload = JSON.parse(Buffer.from(token, 'base64').toString());
+    if (new Date() > new Date(payload.exp)) { return; }
+
+    users
+      .find({ id: user.id })
+      .assign({ verified: true })
+      .write()
+
+    const rcon = await Rcon.instance();
+    const response = await rcon.send(`whitelist add ${user.userName}`);
+
+    if (!response.includes('already') || response.includes('Added')) {
       throw new CustomError('RCON_ERR', 500);
     }
+
+    return;
 
   } catch (err) {
     throw err;
@@ -47,5 +102,6 @@ const register = async (data) => {
 };
 
 module.exports = {
-  register
+  register,
+  confirmAccount
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const account = require('../controllers/account');
 
+const { SITE_URL } = process.env;
+
 const router = (app) => {
 
   app.get('/', (req, res) => {
@@ -9,6 +11,15 @@ const router = (app) => {
   app.post('/account/register', async (req, res, next) => {
     try {
       res.json(await account.register(req.body));
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  app.get('/account/verify/:token', async (req, res, next) => {
+    try {
+      await account.confirmAccount(req.params.token);
+      res.redirect(301, SITE_URL);
     } catch (err) {
       next(err);
     }
