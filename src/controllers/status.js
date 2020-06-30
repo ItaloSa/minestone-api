6fbf38e84b719242efc17d47ddd211a7c93e56c4
Italo Sousa <italusousa@gmail.com>
2020-06-29 23:56:04 -0300

    refactor status endpoint

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -1,9 +1,19 @@
 const { getStatus } = require('mc-server-status');
 
-const serverStatus = () => {
+const serverStatus = async () => {
   try {
-    return getStatus("127.0.0.1");
+    const { players } = await getStatus("127.0.0.1");
+    return {
+      online: true,
+      players: {
+        max: players.max,
+        online: players.online,
+        list: players.sample ?
+          players.sample.map(({name}) => name) : []
+      }
+    }
   } catch (e) {
+    if (e.code && e.code === 'ECONNREFUSED') return { online: false }
     throw e;
   }
 };
